fa3dffb52f5ca75840a8215179c6084df66f7ff8
Folasayo Samuel <folasayosamuel@gmail.com>
2022-09-18 19:41:58 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FloatPointNumbers
+{
+	class Program
+	{
+		static void Main(string[]args)
+		{
+			float number1, number2, product;
+
+			Console.Write("Enter a first number: ");
+			number1 = Convert.ToSingle(Console.ReadLine());
+			
+			Console.Write("Enter a second number: ");
+			number2 = Convert.ToSingle(Console.ReadLine());
+
+			product = number1 * number2;
+
+            Console.WriteLine("{0} * {1} = {2}", number1, number2, product);
+
+            Console.ReadKey();
+		}
+	}
+}
